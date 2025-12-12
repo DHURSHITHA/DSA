96e256d93c9164fa799e4400c501537eed8da1db
DHURSHITHA <dhurshitiha.n2006@gmail.com>
2025-12-12 06:19:13 +0530

    best time to buy and sell teh stock

---
+++ Arrays/Best_time_to_buy_and_sell_stock(121).java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0],sell=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]<min)
+            {
+                min=prices[i];
+            }
+            else
+            {
+                if(prices[i]-min>sell)
+                {
+                    sell=prices[i]-min;
+                }
+            }
+        }
+        return sell;
+        
+    }
+}
