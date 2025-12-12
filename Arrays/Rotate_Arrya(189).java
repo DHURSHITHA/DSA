a7970087aa9b42656a0ee6b5b1029cd829ac317b
DHURSHITHA N <dhurshitha.n2006@gmail.com>
2025-12-12 06:29:01 +0530

    rotate array

---
+++ Arrays/Rotate_Arrya(189).java
@@ -0,0 +1,18 @@
+class Solution {
+    void reverse(int start,int end,int[] nums)
+    {
+        while(start<end)
+        {
+            int temp=nums[start];
+            nums[start++]=nums[end];
+            nums[end--]=temp;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+      k=k%nums.length;
+      if(k==0) return;
+      reverse(0,nums.length-1,nums);
+      reverse(0,k-1,nums);
+      reverse(k,nums.length-1,nums);
+    }
+}
